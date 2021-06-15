fb1ea7e74fb9778029b11c34dcba4e244dd456e3
Taru Saloheimo <taru.saloheimo@gmail.com>
2021-06-15 14:58:54 +0300

    3.14: puhelinluettelo ja tietokanta, step2

--- index.js
+++ index.js
@@ -14,42 +14,6 @@ morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-
-/*
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4
-  },
-  {
-    name: "Joulu Muori",
-    number: "13838383",
-    id: 6
-  },
-  {
-    name: "Pupu Tupuna",
-    number: "123123",
-    id: 8
-  }
-]  
-*/
-
 app.get('/', (req, res) => {
   res.send('<h1>Hello there!</h1>')
 })
@@ -59,23 +23,13 @@ app.get('/info', (req, res) => {
 
 })
 
-/*
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
-})
-*/
-
-
 app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
-    console.log(persons)
+    //console.log(persons)
     res.json(persons)
   })
   
 })
-
-
-
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
@@ -89,7 +43,7 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-/*
+
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   
@@ -98,15 +52,9 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 
 })
-*/
-app.delete('/api/persons/:id', (req, res, next) => {
-  Note.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end()
-    })
-    .catch(error => next(error))
-})
 
+
+/*
 app.post('/api/persons', (req, res) => {
   const person = req.body
 
@@ -127,6 +75,32 @@ app.post('/api/persons', (req, res) => {
 
   res.json(person)
 })
+*/
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+
+  if (body.name === '' || body.number === '') {
+    return res.status(400).json({
+      error: 'name or number is missing!'
+    })
+  }
+  //KÄSITTELE TUPLAHENKILÖ-ERROR
+
+  const person = new Person ({
+    name: body.name,
+    number: body.number,
+  })
+  
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
+
+})
+
+
+
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
