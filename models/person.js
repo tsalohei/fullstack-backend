ff69c13428fa84c4c6587eb94b521c4ad5391506
Taru Saloheimo <taru.saloheimo@gmail.com>
2021-06-16 12:34:28 +0300

    3.21 tietokantaa käyttävä versio internettiin

--- models/person.js
+++ models/person.js
@@ -14,8 +14,8 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: {type: String, unique: true},
-  number: String,
+  name: {type: String, minlength: 3, unique: true},
+  number: {type: String, minlength: 8}
 })
   
 personSchema.set('toJSON', {
